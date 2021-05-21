e1504b7b305efeccf67c19e20b399885b9e5da35
Luk0ss <zaidimams.tik@gmail.com>
2021-05-21 16:16:29 +0300

    feat: added login form for front-end with Vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,12 +22,14 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('login-form', require('./components/Login.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
 
 const app = new Vue({
     el: '#app',
---
+++ resources/js/components/Login.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <b-button v-b-modal.modal-1 variant="primary">Login</b-button>
+
+        <b-modal id="modal-1" title="Login">
+            <div>
+                <b-form id="loginForm" @submit.prevent="onSubmit" v-if="show">
+                    <b-form-group
+                        id="input-group-1"
+                        label="Email address:"
+                        label-for="input-1"
+                        description="We'll never share your email with anyone else."
+                    >
+                        <b-form-input
+                            id="input-1"
+                            v-model="form.email"
+                            type="email"
+                            placeholder="Enter email"
+                            required
+                        ></b-form-input>
+                    </b-form-group>
+
+                    <b-form-group id="input-group-2" label="Your Password:" label-for="input-2">
+                        <b-form-input
+                            id="input-2"
+                            v-model="form.password"
+                            type="password"
+                            placeholder="Enter password"
+                            required
+                        ></b-form-input>
+                    </b-form-group>
+
+                    <div>
+                        <b-form-checkbox v-model="form.checked" name="check-button" switch>
+                            Remember me on this PC
+                        </b-form-checkbox>
+                    </div>
+                </b-form>
+            </div>
+            <template #modal-footer>
+                <b-button type="submit" form="loginForm" variant="primary">Submit</b-button>
+            </template>
+        </b-modal>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+            form: {
+                email: '',
+                password: '',
+                checked: false,
+            },
+            foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+            show: true
+        }
+    },
+    methods: {
+        onSubmit(event) {
+            event.preventDefault()
+            alert(JSON.stringify(this.form))
+        },
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>

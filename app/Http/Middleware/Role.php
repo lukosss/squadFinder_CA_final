8063cab96272a8b932b0a8ba588d872c2cc24edc
Luk0ss <zaidimams.tik@gmail.com>
2021-06-21 13:06:11 +0300

    fix: middleware redirect

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -19,12 +19,6 @@ class Role
         if($user->isAdmin())
             return $next($request);
 
-        foreach($roles as $role) {
-            // Check if user has the role This check will depend on how your roles are set up
-            if($user->hasRole($role))
-                return $next($request);
-        }
-
         return redirect('login');
     }
 }
